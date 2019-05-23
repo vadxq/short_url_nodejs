d451e5dc703f34292720be8567202ebcd42b8f16
vadxq <dongxianlin@vadxq.com>
2019-05-23 11:45:42 +0800

    update readme

--- server/mongo/index.js
+++ server/mongo/index.js
@@ -43,7 +43,7 @@ class Mongodb {
   // 添加
   async save(body) {
     // 随机生成3-6位
-    let str = await this.randomUrl(false, 3, 6)
+    let str = await this.randomUrl(true, 3, 6)
     let isHas = await this.query(str)
     if (isHas.status) {
      this.save(body)
@@ -61,13 +61,13 @@ class Mongodb {
     }
   }
 
-  async randomUrl (randomFlag, min, max) {
+  async randomUrl (isRandom, min, max) {
     let str = '',
         range = min,
         arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
  
     // 随机产生
-    if(randomFlag){
+    if(isRandom){
         range = Math.round(Math.random() * (max-min)) + min;
     }
     for(let i=0; i<range; i++){
